766ed55fee1d34a5c1037226d41cfdae62a9b863
FENG Zhichao <flankerfc@gmail.com>
2015-04-16 18:19:06 +1000

    Initial Commit

---
+++ daigou/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  daigou
+//
+//  Created by Feng Zhichao on 4/16/15.
+//  Copyright (c) 2015 Feng Zhichao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
